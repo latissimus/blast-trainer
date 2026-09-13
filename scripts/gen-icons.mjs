ae0ebf07893945e7b8ae57c5b9150668d927ad1a
flrn <flrn.rau@gmail.com>
2026-09-13 15:37:10 +0200

    u

--- scripts/gen-icons.mjs
+++ scripts/gen-icons.mjs
@@ -5,7 +5,7 @@
 import { Resvg } from '@resvg/resvg-js';
 import { readFileSync, writeFileSync } from 'node:fs';
 
-const svg = readFileSync('LOGSYMBOL.svg', 'utf8');
+const svg = readFileSync('LOGMANAPPSYMBOL.svg', 'utf8');
 
 const targets = [
   ['public/icon-512.png', 512],
